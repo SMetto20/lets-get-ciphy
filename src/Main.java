b450e4ab8140c3a65cf80d5603604caefba101c6
SMetto20 <smetto20@gmail.com>
2022-04-23 13:08:46 +0300

    ft-adds test to see if input is correct

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args)  {
+    public ArrayList<Character> checkLetter(char letsCheck){
+        ArrayList<Character> check = new ArrayList<Character>();
+        return null;
+    }
+    public static void main(String[] args) {
 
     }
-}
+    }
+
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,5 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+public class MainTest {
+    @Test
+    public void checkLetter_stringInput_ArrayList(){
+        Main main = new Main();
+        List<Character>expectedOutput = new ArrayList<Character>();
+        expectedOutput.add('A');
+        assertEquals(expectedOutput, main.checkLetter('a'));
+    }
 
-class MainTest {
 
 }
