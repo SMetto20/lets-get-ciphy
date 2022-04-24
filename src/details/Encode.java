d32f14a17a620ad6dfa5957e75d9a3e3e0dd5f70
SMetto20 <smetto20@gmail.com>
2022-04-24 10:04:01 +0300

    ft-adds parameters for encrypting a string

--- src/details/Encode.java
+++ src/details/Encode.java
@@ -6,19 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Encode {
-    String[] alphabets = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-            "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    public static String encrypt(String text, int key ){
 
-
-    public static void main(String[] args) throws Exception {
-        List<String> alphabets = new ArrayList<>();
-        System.out.println("Kindly enter a phrase");
-        InputStreamReader is = new InputStreamReader(System.in);
-        BufferedReader br  = new BufferedReader(is);
-
-        String phrase = br.readLine();
-        System.out.println(phrase);
     }
+
+
 
 }
 
