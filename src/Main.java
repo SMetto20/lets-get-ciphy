7f3bb769e3a0cfa2fb8b4b0df7be562ebc7c0b44
SMetto20 <smetto20@gmail.com>
2022-04-24 10:38:53 +0300

    encorporates encrypt and decrypt logic to main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+
+import static details.Decode.decrypt;
+import static details.Encode.encrypt;
 
 public class Main {
     public ArrayList<String> encodeWords(String encodeCheck) {
@@ -18,7 +23,21 @@ public class Main {
                 return check2;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br  = new BufferedReader(is);
+        System.out.println("enter text to encrypt");
+        String text = br.readLine();
+        System.out.println("enter encryption key");
+        int key = Integer.parseInt(br.readLine());
+//encrypt
+        System.out.println("Plain Text:\n"+text );
+        String cipher = encrypt(text,key);
+        System.out.println("Cipher Text:\n"+cipher);
+//        decrypt
+        System.out.println("Cipher Text:\n" + text );
+        text =decrypt(cipher,key);
+        System.out.println("Plain Text:\n"+text);
 
     }
 }
--- src/details/Decode.java
+++ src/details/Decode.java
@@ -1,17 +1,30 @@
 package details;
 
 public class Decode {
-    public static String decrypt(String cipher, int key ){
-        for (int i =0; i <cipher.length();i++ ) {
+    public static String decrypt(String cipher, int key) {
+        String text = " ";
+        for (int i = 0; i < cipher.length(); i++) {
             char letter = cipher.charAt(i);
             if (Character.isUpperCase(letter)) {
                 int x = letter - 'A' - key;
-                if (x<0){
-                    x += 26;}
+                if (x < 0) {
+                    x += 26;
+                }
 
                 letter = (char) (x + 'A');
 
-            }
-    }
+            } else if (Character.isLowerCase(letter)) {
+                int x = letter - 'a' - key;
+                if (x < 0) {
+                    x += 26;
+                }
 
+                letter = (char) (x + 'a');
+
+            }
+            cipher += letter;
+
+        }
+        return text;
+    }
 }
