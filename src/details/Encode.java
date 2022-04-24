0438e2364d15fe209ef0edaf7b56b7a54b481aed
SMetto20 <smetto20@gmail.com>
2022-04-24 10:07:24 +0300

    ft- adds code to loop through the text

--- src/details/Encode.java
+++ src/details/Encode.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public class Encode {
     public static String encrypt(String text, int key ){
-
+        String cipher = " ";
+        for (int i =0; i <text.length();i++ ){
+            char letter = text.charAt(i);
     }
 
 
