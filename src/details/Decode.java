4241caf5677dfd7dbbf0af73e628225f25e0926c
SMetto20 <smetto20@gmail.com>
2022-04-24 10:18:22 +0300

    adds loop to go over cipher text

--- src/details/Decode.java
+++ src/details/Decode.java
@@ -1,6 +1,9 @@
 package details;
 
 public class Decode {
-    public static String decrypt(String cipher, int key ){}
+    public static String decrypt(String cipher, int key ){
+        for (int i =0; i <cipher.length();i++ ) {
+            char letter = cipher.charAt(i);
+    }
 
 }
