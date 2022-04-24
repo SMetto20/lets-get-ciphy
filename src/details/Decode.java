fd07b987a1ed7656258595fce360f1e62a058d04
SMetto20 <smetto20@gmail.com>
2022-04-24 10:21:30 +0300

    adds parameters for decrypting uppercase strings

--- src/details/Decode.java
+++ src/details/Decode.java
@@ -4,6 +4,14 @@ public class Decode {
     public static String decrypt(String cipher, int key ){
         for (int i =0; i <cipher.length();i++ ) {
             char letter = cipher.charAt(i);
+            if (Character.isUpperCase(letter)) {
+                int x = letter - 'A' - key;
+                if (x<0){
+                    x += 26;}
+
+                letter = (char) (x + 'A');
+
+            }
     }
 
 }
