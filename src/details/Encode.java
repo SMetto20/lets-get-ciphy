ce37653ed4426c01e905d493e27411893716183f
SMetto20 <smetto20@gmail.com>
2022-04-24 10:11:15 +0300

    ft-adds parameters for text that is in upper case

--- src/details/Encode.java
+++ src/details/Encode.java
@@ -6,12 +6,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Encode {
-    public static String encrypt(String text, int key ){
+    public static String encrypt(String text, int key ) {
         String cipher = " ";
-        for (int i =0; i <text.length();i++ ){
+        for (int i = 0; i < text.length(); i++) {
             char letter = text.charAt(i);
-    }
+            if (Character.isUpperCase(letter)) {
+                int x = letter - 'A' + key;
 
+                x %= 26;
+
+                letter = (char) (x + 'A');
+
+            }
+        }
+    }
 
 
 }
